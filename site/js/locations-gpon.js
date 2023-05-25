113f4f399d5d1a1cd90e6f33d5ed31891264a17b
dudu15051505 <gustavoramos1234@gmail.com>
2023-05-24 21:02:50 -0300

    correção variaveis

--- site/js/locations-gpon.js
+++ site/js/locations-gpon.js
@@ -1,4 +1,4 @@
-var locations-gpon = [
+var locations_gpon = [
 /* CIDADES ADICIONADAS MANUALMENTE, POR ERRO NO CADASTRO DE CONSULTA API DA CLARO */
 { name: 'Dois Irmãos, RS <br> GPON', color: 'green', latitude: '-29.58356', longitude: '-51.089776' },
 
--- site/js/locations-sobrepo.js
+++ site/js/locations-sobrepo.js
@@ -1,4 +1,4 @@
-var locations-sobrepo = [
+var locations_sobrepo = [
 /* CIDADES ADICIONADAS MANUALMENTE, POR ERRO NO CADASTRO DE CONSULTA API DA CLARO */
 { name: 'Mogi das Cruzes, SP <br> Sobreposição HFC e GPON', color: 'yellow', latitude: '-23.52082', longitude: '-46.18541' },
 
