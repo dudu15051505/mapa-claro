0ebdd5f42a859c85e8fa8a2762451bee35f73df3
dudu15051505 <gustavoramos1234@gmail.com>
2023-07-07 19:18:48 -0300

    correção script copiar js

--- scripts/copiar-js-site.ps1
+++ scripts/copiar-js-site.ps1
@@ -20,7 +20,7 @@ Copy-Item "$diretorio_trabalho/js/locations-nada.js" -Destination "$diretorio_tr
 Copy-Item "$diretorio_trabalho/js/locations-neutrogpon.js" -Destination "$diretorio_trabalho/bk/$DateStr/locations-neutrogpon.js" -Recurse -force
 Copy-Item "$diretorio_trabalho/js/locations-neutrohfc.js" -Destination "$diretorio_trabalho/bk/$DateStr/locations-neutrohfc.js" -Recurse -force
 Copy-Item "$diretorio_trabalho/js/locations-sobrepo.js" -Destination "$diretorio_trabalho/bk/$DateStr/locations-sobrepo.js" -Recurse -force
-Copy-Item "$diretorio_trabalho/js/locations-sobrepo.js" -Destination "$diretorio_trabalho/bk/$DateStr/data-lista.txt" -Recurse -force
+Copy-Item "$diretorio_trabalho/js/data-lista.txt" -Destination "$diretorio_trabalho/bk/$DateStr/data-lista.txt" -Recurse -force
 
 Copy-Item "$diretorio_trabalho/js/data-update.js" -Destination "$diretorio_site/js/data-update.js" -Recurse -force
 Copy-Item "$diretorio_trabalho/js/locations-erroapi.js" -Destination "$diretorio_site/js/locations-erroapi.js" -Recurse -force
@@ -30,7 +30,7 @@ Copy-Item "$diretorio_trabalho/js/locations-nada.js" -Destination "$diretorio_si
 Copy-Item "$diretorio_trabalho/js/locations-neutrogpon.js" -Destination "$diretorio_site/js/locations-neutrogpon.js" -Recurse -force
 Copy-Item "$diretorio_trabalho/js/locations-neutrohfc.js" -Destination "$diretorio_site/js/locations-neutrohfc.js" -Recurse -force
 Copy-Item "$diretorio_trabalho/js/locations-sobrepo.js" -Destination "$diretorio_site/js/locations-sobrepo.js" -Recurse -force
-Copy-Item "$diretorio_trabalho/js/locations-sobrepo.js" -Destination "$diretorio_site/js/data-lista.txt" -Recurse -force
+Copy-Item "$diretorio_trabalho/js/data-lista.txt" -Destination "$diretorio_site/js/data-lista.txt" -Recurse -force
 
 Copy-Item "$diretorio_trabalho/js/data-update.js" -Destination "$diretorio_site/js/old/$DateStr/data-update.js" -Recurse -force
 Copy-Item "$diretorio_trabalho/js/locations-erroapi.js" -Destination "$diretorio_site/js/old/$DateStr/locations-erroapi.js" -Recurse -force
